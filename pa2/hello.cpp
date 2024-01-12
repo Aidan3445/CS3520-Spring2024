d3fc19c86cb8b4bb76538e39f8bb74e849bc239e
Aidan Weinberg <weinberg.ai@northeastern.edu>
2024-01-11 20:45:26 -0500

    fix vim formatting

--- pa2/hello.cpp
+++ pa2/hello.cpp
@@ -1,19 +1,19 @@
 /*
- Aidan Weinberg
- 1/9/2024
- PA2 - Intro to git/linux 
- Print your name in ASCII
-*/
+   Aidan Weinberg
+   1/9/2024
+   PA2 - Intro to git/linux 
+   Print your name in ASCII
+   */
 
 #include <iostream>
 using namespace std;
 
 int main(void)
 {
- // Using "Elite" font found here: https://patorjk.com/software/taag/#p=display&f=Elite&t=Aidan%20Weinberg	
- cout<<" ▄▄▄· ▪  ·▄▄▄▄   ▄▄▄·  ▐ ▄     ▄▄▌ ▐ ▄▌▄▄▄ .▪   ▐ ▄ ▄▄▄▄· ▄▄▄ .▄▄▄   ▄▄ •"<<endl;
- cout<<"▐█ ▀█ ██ ██▪ ██ ▐█ ▀█ •█▌▐█    ██· █▌▐█▀▄.▀·██ •█▌▐█▐█ ▀█▪▀▄.▀·▀▄ █·▐█ ▀ ▪"<<endl;
- cout<<"▄█▀▀█ ▐█·▐█· ▐█▌▄█▀▀█ ▐█▐▐▌    ██▪▐█▐▐▌▐▀▀▪▄▐█·▐█▐▐▌▐█▀▀█▄▐▀▀▪▄▐▀▀▄ ▄█ ▀█▄"<<endl;
- cout<<"▐█ ▪▐▌▐█▌██. ██ ▐█ ▪▐▌██▐█▌    ▐█▌██▐█▌▐█▄▄▌▐█▌██▐█▌██▄▪▐█▐█▄▄▌▐█•█▌▐█▄▪▐█"<<endl;
- cout<<" ▀  ▀ ▀▀▀▀▀▀▀▀•  ▀  ▀ ▀▀ █▪     ▀▀▀▀ ▀▪ ▀▀▀ ▀▀▀▀▀ █▪·▀▀▀▀  ▀▀▀ .▀  ▀·▀▀▀▀ "<<endl;
+    // Using "Elite" font found here: https://patorjk.com/software/taag/#p=display&f=Elite&t=Aidan%20Weinberg	
+    cout<<" ▄▄▄· ▪  ·▄▄▄▄   ▄▄▄·  ▐ ▄     ▄▄▌ ▐ ▄▌▄▄▄ .▪   ▐ ▄ ▄▄▄▄· ▄▄▄ .▄▄▄   ▄▄ •"<<endl;
+    cout<<"▐█ ▀█ ██ ██▪ ██ ▐█ ▀█ •█▌▐█    ██· █▌▐█▀▄.▀·██ •█▌▐█▐█ ▀█▪▀▄.▀·▀▄ █·▐█ ▀ ▪"<<endl;
+    cout<<"▄█▀▀█ ▐█·▐█· ▐█▌▄█▀▀█ ▐█▐▐▌    ██▪▐█▐▐▌▐▀▀▪▄▐█·▐█▐▐▌▐█▀▀█▄▐▀▀▪▄▐▀▀▄ ▄█ ▀█▄"<<endl;
+    cout<<"▐█ ▪▐▌▐█▌██. ██ ▐█ ▪▐▌██▐█▌    ▐█▌██▐█▌▐█▄▄▌▐█▌██▐█▌██▄▪▐█▐█▄▄▌▐█•█▌▐█▄▪▐█"<<endl;
+    cout<<" ▀  ▀ ▀▀▀▀▀▀▀▀•  ▀  ▀ ▀▀ █▪     ▀▀▀▀ ▀▪ ▀▀▀ ▀▀▀▀▀ █▪·▀▀▀▀  ▀▀▀ .▀  ▀·▀▀▀▀ "<<endl;
 }
