2c7dce07be052fb618bfcc8a9135773e3529cb47
Aidan Weinberg <aidan3445@gmail.com>
2024-01-14 13:55:27 -0500

    autoformatter working

--- pa2/pa2.cpp
+++ pa2/pa2.cpp
@@ -1,7 +1,7 @@
 /*
    Aidan Weinberg
    1/9/2024
-   PA2 - Intro to git/linux 
+   PA2 - Intro to git/linux
    Print your name in ASCII
    */
 
@@ -10,7 +10,7 @@ using namespace std;
 
 int main(void)
 {
-    // Using "Elite" font found here: https://patorjk.com/software/taag/#p=display&f=Elite&t=Aidan%20Weinberg	
+    // Using "Elite" font found here: https://patorjk.com/software/taag/#p=display&f=Elite&t=Aidan%20Weinberg
     cout<<" ▄▄▄· ▪  ·▄▄▄▄   ▄▄▄·  ▐ ▄     ▄▄▌ ▐ ▄▌▄▄▄ .▪   ▐ ▄ ▄▄▄▄· ▄▄▄ .▄▄▄   ▄▄ •"<<endl;
     cout<<"▐█ ▀█ ██ ██▪ ██ ▐█ ▀█ •█▌▐█    ██· █▌▐█▀▄.▀·██ •█▌▐█▐█ ▀█▪▀▄.▀·▀▄ █·▐█ ▀ ▪"<<endl;
     cout<<"▄█▀▀█ ▐█·▐█· ▐█▌▄█▀▀█ ▐█▐▐▌    ██▪▐█▐▐▌▐▀▀▪▄▐█·▐█▐▐▌▐█▀▀█▄▐▀▀▪▄▐▀▀▄ ▄█ ▀█▄"<<endl;
