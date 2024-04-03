ba634cb810822685f4f0798c9758326290fb3826
Aidan Weinberg <weinberg.ai@northeastern.edu>
2024-04-03 10:22:41 -0400

    4

--- pa13/DebugT-4.cpp
+++ pa13/DebugT-4.cpp
@@ -10,81 +10,75 @@
 #include <iostream>
 
 using namespace std;
-// class to perform various operations on two variables of type T
-template <class T>
-class Calculator
-{
-private:
-    T num1;
-    T num2;
 
-public:
-    Calculator(T n1, T n2);
-    T add();
-    T subtract();
-    T multiply();
-    T divide();
-    bool isgreater();
+// class to perform various operations on two variables of type T
+template<class T>
+class Calculator {
+  private:
+	T num1;
+	T num2;
+
+  public:
+	Calculator(T n1, T n2);
+	T add();
+	T subtract();
+	T multiply();
+	T divide();
+	bool isgreater();
 };
 
-template <class T>
-Calculator<T>::Calculator(T n1, int n2)
-{
-    num1 = n1;
-    num2 = n2;
+template<class T>
+Calculator<T>::Calculator(T n1, T n2) {	 // int -> T
+	num1 = n1;
+	num2 = n2;
 }
 
-template <class T>
-Calculator<T>::add()
-{
-    return num1 + num2;
+template<class T>
+T Calculator<T>::add() {  // add return type T
+	return num1 + num2;
 }
 
-template <class T>
-Calculator<T>::subtract()
-{
-    return num1 - num2;
+template<class T>
+T Calculator<T>::subtract() {  // add return type T
+	return num1 - num2;
 }
 
-template <class T>
-T Calculator<T>::multiply(T n1, T n2)
-{
-    return n1 * n2;
+template<class T>
+T Calculator<T>::multiply() {  // remove params
+	return num1 * num2;		   // use class members
 }
 
-template <class T>
-T Calculator<T>::divide()
-{
-    return num1 % num2;
+template<class T>
+T Calculator<T>::divide() {
+	return num1 / num2;	 // % -> /
 }
 
-template <class T>
-T Calculator<T>::isgreater()
-{
-    if (num1 > num2)
-        return true;
-    else
-        return false;
+template<class T>
+bool Calculator<T>::isgreater() {  // return type bool
+	if (num1 > num2)
+		return true;
+	else
+		return false;
 }
 
-int main()
-{
-    Calculator<int> calc(10, 4); // int operations
-    cout << calc.add() << endl;
-    cout << calc.subtract() << endl;
-    cout << calc.multiply() << endl;
-    cout << calc.divide() << endl;
-    cout << std::boolalpha << calc.isgreater() << endl;
+int main() {
+	Calculator<int> calc(10, 4);  // int operations
+	cout << calc.add() << endl;
+	cout << calc.subtract() << endl;
+	cout << calc.multiply() << endl;
+	cout << calc.divide() << endl;
+	cout << std::boolalpha << calc.isgreater() << endl;
 
-    Calculator<double> calc2(10.234, 4.235); // double operations
-    cout << calc2.add() << endl;
-    cout << calc2.subtract() << endl;
-    cout << calc2.multiply() << endl;
-    cout << calc2.divide() << endl;
-    cout << std::boolalpha << calc2.isgreater() << endl;
+	Calculator<double> calc2(10.234, 4.235);  // double operations
+	cout << calc2.add() << endl;
+	cout << calc2.subtract() << endl;
+	cout << calc2.multiply() << endl;
+	cout << calc2.divide() << endl;
+	cout << std::boolalpha << calc2.isgreater() << endl;
 
-    Calculator<string> calc3("Hello", 1); // string operations
-    cout << calc3.add() << endl;
-    cout << std::boolalpha << calc3.isgreater() << endl;
-    return 0;
+	// string operations
+	Calculator<string> calc3("Hello", "Goodbye");  // 1 -> "goodbye" (string)
+	cout << calc3.add() << endl;
+	cout << std::boolalpha << calc3.isgreater() << endl;
+	return 0;
 }
